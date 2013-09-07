ac3daf370edbfc857ab1aff122ff5a46a6df77a1
Shane Kuester <shout@shanekuester.com>
2013-09-07 14:28:38 -0500

    adds optimization filters

--- rehab/engine.rb
+++ rehab/engine.rb
@@ -1,15 +1,15 @@
 module Rehab
 	class Engine < Temple::Engine
-		use ::Rehab::Parser
-		use ::Rehab::Interpolation
-		use ::Rehab::DoFilter
-		use ::Rehab::Control
+		use Rehab::Parser
+		use Rehab::Interpolation
+		use Rehab::DoFilter
+		use Rehab::Control
 
-		# filter :MultiFlattener
-		# filter :StaticMerger
-		# filter :DynamicInliner
-		filter :Escapable
-		filter :ControlFlow
-		generator :ArrayBuffer, :newline
+		filter :MultiFlattener
+		filter :StaticMerger
+		filter :DynamicInliner
+		# filter :Escapable
+		# filter :ControlFlow
+		generator :ArrayBuffer
 	end
 end
